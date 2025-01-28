c10a9ef0954361f8c6727587f4b0845ebcfe8baf
Mariusz <devrogm@gmail.com>
2025-01-28 20:26:38 +0100

    Add example of local_settings

---
+++ server/local_settings.example.py
@@ -0,0 +1,18 @@
+#=================================
+# SECRET KEY
+#=================================
+SECRET_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+# =================================
+# DATA BASE - POSTGRESQL
+# =================================
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'XXXXXXXXXXXXXXXXXXXXXXXXXXX',
+        'USER': 'XXXXXXXXXXXXXXXXXXXXXXXXXXX',
+        'PASSWORD': 'XXXXXXXXXXXXXXXXXXXXXXXXXXX',
+        'HOST': 'XXXXXXXXXXXXXXXXXXXXXXXXXXX',
+        'PORT': 'XXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    }
+}
